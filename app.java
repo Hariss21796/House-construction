c49424fb3e269083a97c96c0116e8d0e23505ff6
haris siddiqui <haaris21796@gmail.com>
2021-09-04 23:11:01 +0530

    adding file app.java

---
+++ app.java
@@ -0,0 +1,13 @@
+package com.haris.java2;
+
+public class Animal {
+
+	public static void main(String[] args) {
+		getName("Max");
+	}
+
+	public static void getName(String name) {
+		System.out.println("Animal name is:" + name);
+
+	}
+}
